d5e2b7d085a48c610ca18efef44524f9704c3077
dodadoa <maddodadoa@gmail.com>
2018-06-03 05:05:39 +0700

    :sparkles: implement Triangle Exercises

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,31 @@ package com.company;
 
 public class Main {
 
+    public static void printAsterisk(int numberOfAsterisk) {
+        for(int asterisk = 1; asterisk <= numberOfAsterisk; asterisk++) {
+            System.out.print('*');
+        };
+    }
+
+    public static void printLineAsterisk(int numberOfLine) {
+        System.out.print('\n');
+        for(int line = 1; line <= numberOfLine; line++) {
+            System.out.println('*');
+        }
+    }
+
+    public static void printRightTriangleAsterisk(int numberOfLine) {
+        for(int line = 1; line <= numberOfLine; line++) {
+            String repeatedAsterisk = new String(new char[line]).replace("\0", "*");
+            System.out.print(repeatedAsterisk + '\n');
+        }
+
+    }
+
     public static void main(String[] args) {
-        system.output
+        System.out.println('*');
+        printAsterisk(3);
+        printLineAsterisk(4);
+        printRightTriangleAsterisk(4);
     }
 }
